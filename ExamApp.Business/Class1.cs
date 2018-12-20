e852bf60a6f4df6e25d4aa9c15efc42fb473425b
Pankaj-Admin <pankajkumarhbti@gmail.com>
2018-12-20 12:34:33 +0530

    First Commit of Exam App

---
+++ ExamApp.Business/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp.Business
+{
+    
+    public class Class1
+    {
+        public string result = string.Empty;
+        dynamic order = new Order().Customer;
+        public void Func(Order order)
+        {
+            var xys = order?.Customer?.Name
+        }
+    }
+
+   public class Order
+    {
+        public Customer Customer { get; set; }
+    }
+
+    public   class Customer
+    {
+        public string Name { get; set; }
+    }
+}
